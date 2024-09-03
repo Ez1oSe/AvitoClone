21bfdec95481ec12526c87908bad00474ed7b533
Daniil <dn.danii@mail.ru>
2024-09-03 23:18:21 +0300

    AvitoClone

---
+++ src/main/java/com/example/avito/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.avito.repositories;
+
+import com.example.avito.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
